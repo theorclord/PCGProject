42e722e4cb9b24beb929f496c53a054675c45d62
theorclord <mikkeltheorclord@hotmail.com>
2015-11-30 15:36:34 +0100

    added player movement, doors, walls, and logic for expanding dungeon

---
+++ DungeonGenerator/Assets/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour {
+    private GameObject mainCamera;
+    private Board.MAP_REF[,] dungeon;
+    private GameController gameCon;
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            executeMove(Vector3.up);    
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            executeMove(Vector3.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            executeMove(Vector3.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            executeMove(Vector3.right);
+        }
+    }
+
+    public void SetPlayerCamera(GameObject cam)
+    {
+        mainCamera = cam;
+    }
+
+    public void SetDungeon(Board.MAP_REF[,] dun)
+    {
+        dungeon = dun;
+    }
+
+    private void executeMove(Vector3 vec)
+    {
+        //Debug.Log(dungeon[(int)transform.position.x, (int)transform.position.y]);
+        int transXTrans = (int)(transform.position.x+vec.x);
+        int transYTrans = (int)(transform.position.y + vec.y);
+        if (!(dungeon.GetLongLength(0) <= transXTrans || 0 > transXTrans || dungeon.GetLongLength(1) <= transYTrans || transYTrans < 0))
+        {
+            if (dungeon[(int)(transform.position.x + vec.x), (int)(transform.position.y + vec.y)] == Board.MAP_REF.WALL)
+            {
+                Debug.Log("Wall, can't move through");
+            }
+            else if(dungeon[(int)(transform.position.x + vec.x), (int)(transform.position.y + vec.y)] == Board.MAP_REF.DOOR)
+            {
+                gameCon.OpenDoor(transform.position);
+                transform.Translate(vec);
+            }
+            else
+            {
+                transform.Translate(vec);
+            }
+        }
+        mainCamera.transform.position = transform.position;
+    }
+
+    public void SetGameController(GameController gameController)
+    {
+        gameCon = gameController;
+    }
+}
