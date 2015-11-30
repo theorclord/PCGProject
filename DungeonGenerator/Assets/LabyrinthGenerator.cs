0ec4541f86e934785792ca0029608703fc921bf8
theorclord <mikkeltheorclord@hotmail.com>
2015-11-30 11:57:24 +0100

    Removed unused scene

--- DungeonGenerator/Assets/LabyrinthGenerator.cs
+++ DungeonGenerator/Assets/LabyrinthGenerator.cs
@@ -7,14 +7,14 @@ public class LabyrinthGenerator : MonoBehaviour {
 	void Start () {
         string[,] tempLab = { { null, "W", null }, { "W", null, null }, { null, "W", "W" } };
         gameBoard = new GameObject[tempLab.GetLength(0),tempLab.GetLength(1)];
-        Debug.Log(tempLab.GetLength(0) + " " + tempLab.GetLength(1));
+        //Debug.Log(tempLab.GetLength(0) + " " + tempLab.GetLength(1));
         for (int i = 0; i < tempLab.GetLength(0); i++)
         {
             for (int j = 0; j < tempLab.GetLength(1); j++)
             {
                 if(tempLab[i,j] == null)
                 {
-                    Debug.Log("Null hit");
+                    //ToDo logic for no piece
                 } else if(tempLab[i,j] == "W")
                 {
                     GameObject piece = Instantiate(Resources.Load("Prefabs/GamePiece")) as GameObject;
