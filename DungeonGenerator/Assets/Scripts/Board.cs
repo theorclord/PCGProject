9e7946d5f0ce023161f2b61c023bfa13ec5ae7b0
theorclord <mikkeltheorclord@hotmail.com>
2015-12-10 14:15:34 +0100

    fixed minor bug with door opening

--- DungeonGenerator/Assets/Scripts/Board.cs
+++ DungeonGenerator/Assets/Scripts/Board.cs
@@ -60,6 +60,7 @@ public class Board {
     }
 	public bool placeRoom(Room r, int dir, Vector2 ent, ArrayList doors, int numberOfDoors)
 	{
+        Debug.Log("Setting Room");
         bool res = false;
         // from GameController, vector has +1 or -1 offset in the direction.
         //makeRoom(r.startX, r.startY, r.xLength, r.yLength, dir, r, dir);
@@ -230,7 +231,6 @@ public class Board {
             int doorsMade = 0;
             for (int d = 0; d < numDoors; d++)
             {
-                Debug.Log("Array Content" + doors[d]);
                 switch ((int)doors[d])
                 {
                     case 0:
@@ -302,7 +302,6 @@ public class Board {
             int doorsMade = 0;
             for (int d = 0; d < numDoors; d++)
             {
-                Debug.Log("Array Content" + doors[d]);
                 switch ((int)doors[d])
                 {
                     case 0:
